5b47780de130763cb39696a6b95bd0d0d71d62d3
yhnco <65667037+yhnco@users.noreply.github.com>
2021-03-12 13:34:59 -0500

    fixed routes

--- js/router.js
+++ js/router.js
@@ -1,11 +1,11 @@
-import Accueil from './pages/accueil.js'
-import Ville from './pages/ville.js'
-import Meteo from './pages/meteo.js'
+import accueil from './pages/accueil.js'
+import ville from './pages/ville.js'
+import meteo from './pages/meteo.js'
 
 export default new VueRouter({
     routes: [
-        { path: '/', component: Accueil },
-        { path: '/selection', component: Ville },
-        { path: '/meteo/:ville', component: Meteo },
+        { path: '/', component: accueil },
+        { path: '/selection', component: ville },
+        { path: '/meteo/:ville', component: meteo },
     ]
 })
