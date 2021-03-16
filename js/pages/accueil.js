fe3b7511cf5f3edf21d6e08ac38e3782059b4055
yhnco <65667037+yhnco@users.noreply.github.com>
2021-03-16 13:50:36 -0400

    landing page redesign

--- js/pages/accueil.js
+++ js/pages/accueil.js
@@ -8,8 +8,7 @@ export default tpl({
             lang: 'en',
             title: 'Programmeur Junior',
             introduction: 'Toujours à l\'affut des nouvelles technologies, je suis particulièrement interressé par ce qui se trouve sous la couche front-end des applications, logiciels et sites web.',
-            projectDisplay: 'Vous trouverez, en cliquant sur le lien ci-dessous, un exemple d\'une petite application météo utilisant la technologie VueJs dans le but de faire appel à un REST API, traiter et afficher les données reçues.',
-            aboutMe: 'Pour en savoir plus à mon sujet ou me contacter:',
+            projectDisplay: 'Vous trouverez, en cliquant sur le lien, un exemple d\'une petite application météo utilisant la technologie VueJs dans le but de faire appel à un REST API, traiter et afficher les données reçues.',
             seeProject: 'Voir le projet'
         }
     },
@@ -19,15 +18,13 @@ export default tpl({
                 this.lang = 'fr'
                 this.title = 'Junior Programmer'
                 this.introduction = 'Always on the lookout for new technologies, I am particularly interested in what is under the front-end layer of applications, softwares and websites.'
-                this.projectDisplay = 'You will find by clicking on the link below an example of a small weather application using VueJs technology in order to use a REST API, process and display data received.'
-                this.aboutMe = 'To find out more about me or contact me:'
+                this.projectDisplay = 'You will find, by clicking on the link, an example of a small weather application using VueJs technology in order to use a REST API, process and display data received.'
                 this.seeProject = 'See the project'
             } else {
                 this.lang = 'en'
                 this.title = 'Programmeur Junior'
                 this.introduction = 'Toujours à l\'affut des nouvelles technologies, je suis particulièrement interressé par ce qui se trouve sous la couche front-end des applications, logiciels et sites web.'
-                this.projectDisplay = 'Vous trouverez en cliquant sur le lien ci-dessous un exemple d\'une petite application utilisant la technologie VueJs dans le but de faire appel à un REST API, traiter et afficher les données recues.'
-                this.aboutMe = 'Pour en savoir plus à mon sujet ou me contacter:'
+                this.projectDisplay = 'Vous trouverez, en cliquant sur le lien, un exemple d\'une petite application utilisant la technologie VueJs dans le but de faire appel à un REST API, traiter et afficher les données recues.'
                 this.seeProject = 'Voir le projet'
             }
         }
